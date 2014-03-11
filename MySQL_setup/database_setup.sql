89e814e75e538f5de09385ba39c855c83c233fa7
jjuhala <janne.juhala@outlook.com>
2014-03-11 12:13:57 +0200

    Updated DB table setup file

--- MySQL_setup/database_setup.sql
+++ MySQL_setup/database_setup.sql
@@ -1,58 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Mar 09, 2014 at 07:17 PM
--- Server version: 5.5.34
--- PHP Version: 5.4.22-1+debphp.org~precise+1
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `homehub`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hh_actions`
---
 
 CREATE TABLE IF NOT EXISTS `hh_actions` (
   `actionID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
   `commandList` varchar(300) NOT NULL,
   `triggers` varchar(300) NOT NULL,
+  `showOnUI` tinyint(1) NOT NULL,
   `notes` varchar(300) NOT NULL,
   PRIMARY KEY (`actionID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hh_commands`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 CREATE TABLE IF NOT EXISTS `hh_commands` (
   `commandID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
   `command` varchar(50) NOT NULL,
   `ruleList` varchar(300) NOT NULL,
   `notes` varchar(300) NOT NULL,
   PRIMARY KEY (`commandID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hh_data`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 CREATE TABLE IF NOT EXISTS `hh_data` (
   `ID` int(10) unsigned NOT NULL,
@@ -60,12 +32,6 @@ CREATE TABLE IF NOT EXISTS `hh_data` (
   `data` varchar(50) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hh_rules`
---
 
 CREATE TABLE IF NOT EXISTS `hh_rules` (
   `ruleID` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -75,6 +41,17 @@ CREATE TABLE IF NOT EXISTS `hh_rules` (
   `notes` varchar(300) NOT NULL,
   PRIMARY KEY (`ruleID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `hh_sensors` (
+  `sensorID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `cmd_name` varchar(100) NOT NULL,
+  `currentVal` varchar(50) NOT NULL,
+  `lastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `showOnUI` tinyint(1) NOT NULL,
+  `notes` varchar(300) NOT NULL,
+  PRIMARY KEY (`sensorID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
