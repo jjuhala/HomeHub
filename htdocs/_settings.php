3d4bf7e6074b4e6c6071c59fdc4bcede0908a95c
jjuhala <janne.juhala@outlook.com>
2014-03-12 11:21:12 +0200

    Updated settings file

--- htdocs/_settings.php
+++ htdocs/_settings.php
@@ -1,26 +1,39 @@
 <?php
 // HomeHub configuraion file
+//                              ↓  Edit these  ↓
 return array(
-	"notification_email" => "your@email.com",
+    'notification_email' =>     'your@email.com',   /* Email where you want notifications */
 
-	//Connection information
-	"arduino_ip" => "0.0.0.0",
-	"arduino_port" => 80,
-	"arduino_secret" => "your_arduino_server_secret_here",
-	"mysql_ip" => "127.0.0.1",
-	"mysql_port" => 3306,
-	"mysql_database" => "your_mysql_database_for_homehub",
-	"mysql_user" => "your_mysql_user",
-	"mysql_password" => "your_mysql_password",
+    //Connection information
+    'arduino_ip' =>             '0.0.0.0',          /* Server arduino's IP */
+    'arduino_port' =>           80,                 /* Server arduino's port, default: 80 */
+    'arduino_secret' =>         'secret',           /* Secret you set to arduino server */
+    'mysql_ip' =>               '127.0.0.1',        /* IP of MySQL Server, usually 127.0.0.1 (localhost) */
+    'mysql_port' =>             3306,               /* MySQL server port, almost always 3306 */
+    'mysql_database' =>         'database',         /* Database name you created to MySQL server */
+    'mysql_user' =>             'your_mysql_user',  /* Database user you created to MySQL server */
+    'mysql_password' =>         'your_mysql_pw',    /* Database user's password */
 
-	// Available pages
-	"pages_whitelist" => array 	(
-									"home",
-									"manage-rules",
-									"manage-actions"
-								),
+    // Api
+    'api_secret' => 'any_secret_for_api',           /* Select any secret for api, special chars are bad, mmkay? */
 
-	// Api
-	"api_secret" => "any_secret_for_api",
+
+    // 
+    // Do not edit after this line unless you know what you're doing
+    // 
+
+
+
+    // Available pages
+    'pages_whitelist' => array
+    (
+        'home',
+        'manage-rules',
+        'manage-actions',
+        'manage-commands',
+        'manage-sensors'
+    ),
+
+
 );
 ?>
