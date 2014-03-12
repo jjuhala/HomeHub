91068f9321c1ef5bde718cf2f7b97a076bf57a5e
jjuhala <janne.juhala@outlook.com>
2014-03-12 10:29:55 +0200

    Finished Manage-commands page

--- htdocs/inc/inc.manage-commands.php
+++ htdocs/inc/inc.manage-commands.php
@@ -1,103 +1,97 @@
 <?php
-	// Let's see if this was POST request (adding new actions)
+	// Let's see if this was POST request (adding new commands)
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-		// Add new action if POST data for it is available
+		// Add new if POST data for it is available
 		if (isset(
 			$_POST['name'],
-			$_POST['commandList'],
-			$_POST['triggers'],
+			$_POST['command'],
+			$_POST['rules'],
 			$_POST['notes']
 		)) {
 			// All needed vars received, now validate them!
 
 
-			// -- Make sure lenghts match database
+			// -- Make sure lenghts match table's maximums
 
 			// Array of received data and their max lengths
 			$data_len_validate = array 
 			(
 				'name' => 100,
-				'commandList' => 300,
-				'triggers' => 300,
-				'showOnUI' => 1,
+				'command' => 50,
+				'rules' => 300,
 				'notes' => 300
 			);
 
-
-			// Convert checkbox yes/no to tinyint(1)
-			$_POST['showOnUI'] = (isset($_POST['showOnUI']) && $_POST['showOnUI'] == 'on' ? '1' : '0'); 
-
+			// Validate length
 			foreach($data_len_validate as $vdata => $maxlen) {
 				if (strlen($_POST[$vdata]) > $maxlen) {
-					$this->addError('Error adding new Action!',
+					$this->addError('Error adding new Command!',
 							"$vdata is too long! Max length ".
 							"for $vdata is $maxlen characters");
 				}
 			}
 
-			// Make sure there's no spaces in name or lists
+			// List of items which can't contain spaces
 			$data_space_validate = array
 			(
-				'name',
-				'commandList',
-				'triggers',
-				'showOnUI'
+				'command',
+				'rules'
 			);
 
+			// Validate spaceless items
 			foreach($data_space_validate as $vdata) {
 				if (preg_match('/\s/',$_POST[$vdata])) {
-					$this->addError('Error adding new Action!',
+					$this->addError('Error adding new Command!',
 							"$vdata has spaces. $vdata can't have spaces in it!");
 				}
 			}
 
-			// That's it, if no errors so far, add new action
+			// That's it, if no errors so far, add a new command
 			if ($this->showError === false) {
 				$this->query(
-					'INSERT INTO hh_actions '.
-					'(actionID ,name ,commandList ,triggers ,showOnUI ,notes) '.
+					'INSERT INTO hh_commands '.
+					'(commandID,name,command,ruleList,notes) '.
 					'VALUES ('.
 						'NULL,'.
 						'\''.$_POST['name'].'\','. 
-						'\''.$_POST['commandList'].'\','. 
-						'\''.$_POST['triggers'].'\','. 
-						'\''.$_POST['showOnUI'].'\','. 
+						'\''.$_POST['command'].'\','. 
+						'\''.$_POST['rules'].'\','. 
 						'\''.$_POST['notes'].'\');'
 					);
-				$this->addNotice('Success','New action added successfully.');
+				$this->addNotice('Success','New command added successfully.');
 			}
 
 		} else { // Else of: isset (post vars)
 
-			// Not all needed POST variables for adding new action received
+			// Not all needed POST variables for adding new items received
 			// Let's see if we want to delete something
 			if (
 					isset(
 						$_POST['action'],
-						$_POST['action_id']
+						$_POST['id']
 						) 
 					&& 
 					$_POST['action'] == "delete"
 				) {
-				// action is 'delete' and action_id set, this is a delete request
+				// action is 'delete' and id set, this is a delete request
 
 				// Validate input
-				if (ctype_digit($_POST['action_id'])) {
-					// Input ok, delete wanted action
-					$this->query('DELETE FROM hh_actions WHERE actionID='.$_POST['action_id']);
-					$this->addNotice('Success','Action deleted successfully.');
+				if (ctype_digit($_POST['id'])) {
+					// Input ok, delete wanted items
+					$this->query('DELETE FROM hh_commands WHERE commandID='.$_POST['id']);
+					$this->addNotice('Success','Command deleted successfully.');
 				} else {
-					// Delete command reveived, but the action id isn't valid
-					$this->addError('Error deleting Action!',
-							'No valid action_id given'.$_POST['action_id']);
+					// Delete command reveived, but the id isn't valid
+					$this->addError('Error deleting Command!',
+							'No valid id given');
 				}
 			} else {
 				// POST request received but it isn't delete request
 				// and doesn't contain all info for adding new
-				$this->addError('Error adding new Action!',
+				$this->addError('Error adding new Command!',
 								'POST request made, but not all POST variables set'.
-								' for adding new action. No new actions were added.');
+								' for adding new command. No new commands were added.');
 			}
 
 
