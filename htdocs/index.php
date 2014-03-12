2a4d2d0653229b133e1b1633301ccc3e9887fb8f
jjuhala <janne.juhala@outlook.com>
2014-03-12 11:22:51 +0200

    Added & ignored new development settings file
    _addn_settings.php (ignored) can be used for settings during the
    development

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,8 @@
 <?php
 	require('inc/ui.class.php');
 	$ui = new UI();
-	$ui->conf = include('_settings.php');
+	$ui->settings_file = (file_exists('_addn_settings.php') ? '_addn_settings.php' : '_settings.php');
+	$ui->conf = include($ui->settings_file);
 	$ui->pdo = include('inc/func/_connect-mysql.php');
 	$ui->validate_and_render($_GET);
 ?>
